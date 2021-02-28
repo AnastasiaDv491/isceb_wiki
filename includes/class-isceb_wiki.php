256e45590165b5ff4f32173bc278ea9532636a2b
Stijn Pinkhof <stijn.pinkhof@skynet.be>
2021-02-28 18:22:02 +0100

    plugin name

--- includes/class-isceb_wiki.php
+++ includes/class-isceb_wiki.php
@@ -45,9 +45,9 @@ class Isceb_wiki
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 * @var      string    $isceb_wiki    The string used to uniquely identify this plugin.
 	 */
-	protected $plugin_name;
+	protected $isceb_wiki;
 
 	/**
 	 * The current version of the plugin.
@@ -74,7 +74,7 @@ class Isceb_wiki
 		} else {
 			$this->version = '1.0.0';
 		}
-		$this->plugin_name = 'isceb_wiki';
+		$this->isceb_wiki = 'isceb_wiki';
 
 		$this->load_dependencies();
 		$this->set_locale();
@@ -129,26 +129,52 @@ class Isceb_wiki
 		 */
 		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-isceb_wiki-post_types.php';
 
-		// Define path and URL to the ACF plugin.
-		define('MY_ACF_PATH', dirname(__FILE__) . '/advanced-custom-fields/');
-		define('MY_ACF_URL', dirname(__FILE__) . '/advanced-custom-fields/');
-		define('MY_ACF_JSON',dirname(__FILE__) . '/acf-json');
+		/**
+		 * Define path and URL to the ACF plugin.
+		 * Include the ACF plugin.
+		 * Customize the url setting to fix incorrect asset URLs.
+		 * (Optional) Hide the ACF admin menu item.
+		 */
+		define('isceb_wiki_ACF_PATH', plugin_dir_path(dirname(__FILE__)) . '/includes/acf/');
+		define('isceb_wiki_ACF_URL', plugin_dir_url(dirname(__FILE__)) . '/includes/acf/');
 
-		// Include the ACF plugin.
-		include_once(MY_ACF_PATH . 'acf.php');
 
-		// Customize the url setting to fix incorrect asset URLs.
-		// add_filter('acf/settings/url', 'my_acf_settings_url');
 
-		/*acf-fields.json*/
+		include_once(isceb_wiki_ACF_PATH . 'acf.php');
+
+		// add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+
+		add_filter('acf/settings/url', 'isceb_wiki_settings_url');
+		function isceb_wiki_settings_url($url)
+		{
+			return isceb_wiki_ACF_URL;
+		}
+		//add_filter('acf/settings/show_admin', 'isceb_wiki_settings_show_admin');
+		function isceb_wiki_settings_show_admin($show_admin)
+		{
+			return false;
+		}
+
+		add_filter('acf/settings/save_json', 'isceb_wiki_json_save_point');
+		function isceb_wiki_json_save_point($path)
+		{
+			$path = plugin_dir_path(dirname(__FILE__)) . 'acf-json';
+			return $path;
+		}
+
+		add_filter('acf/settings/load_json', 'isceb_wiki_json_load_point',1);
+		function isceb_wiki_json_load_point($paths)
+		{
+			unset($paths[0]);
+			$paths[] = plugin_dir_path(dirname(__FILE__)) . '/acf-json';
+			error_log("ddddddddd");
+			error_log( plugin_dir_path(dirname(__FILE__)) . '/acf-json');
+			return $paths;
+		}
+
+
 		$this->loader = new Isceb_wiki_Loader();
 	}
-
-	
-
-
-	
-
 
 	/**
 	 * Define the locale for this plugin for internationalization.
@@ -178,7 +204,7 @@ class Isceb_wiki
 	{
 		$plugin_post_types = new Isceb_Wiki_Post_Types();
 		$this->loader->add_action('init', $plugin_post_types, 'create_custom_post_type', 999);
-		$plugin_admin = new Isceb_wiki_Admin($this->get_plugin_name(), $this->get_version());
+		$plugin_admin = new Isceb_wiki_Admin($this->get_isceb_wiki(), $this->get_version());
 
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
@@ -186,6 +212,7 @@ class Isceb_wiki
      * Handle POST
      */
 		$this->loader->add_action('admin_post_post_first', $plugin_admin, 'post_first');
+
 		//Turn this on if you also want it for non authenticated users
 		// $this->loader->add_action('admin_post_nopriv_post_first', $plugin_admin, 'post_first');
 	}
@@ -200,7 +227,7 @@ class Isceb_wiki
 	private function define_public_hooks()
 	{
 
-		$plugin_public = new Isceb_wiki_Public($this->get_plugin_name(), $this->get_version());
+		$plugin_public = new Isceb_wiki_Public($this->get_isceb_wiki(), $this->get_version());
 
 		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
 		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
@@ -224,11 +251,6 @@ class Isceb_wiki
 		$this->loader->add_filter('the_content', $plugin_public, 'add_course_to_single_if_phase');
 
 		$this->loader->add_filter('the_content', $plugin_public, 'add_phase_to_single_if_program');
-
-		// ACF: Customize the url setting to fix incorrect asset URLs.
-		$this->loader->add_filter('acf/settings/url', $plugin_public, 'my_acf_settings_url');
-
-		$this->loader->add_filter('acf/settings/load_json',$plugin_public, 'my_acf_json_load_point');
 	}
 
 
@@ -249,9 +271,9 @@ class Isceb_wiki
 	 * @since     1.0.0
 	 * @return    string    The name of the plugin.
 	 */
-	public function get_plugin_name()
+	public function get_isceb_wiki()
 	{
-		return $this->plugin_name;
+		return $this->isceb_wiki;
 	}
 
 	/**
