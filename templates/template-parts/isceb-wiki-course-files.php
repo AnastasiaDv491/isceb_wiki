a80afa093e4dc837f176d6783395da3bd8c38ffe
Stijn Pinkhof <stijn.pinkhof@skynet.be>
2021-08-28 20:50:30 +0200

    new style and structure for file page

--- templates/template-parts/isceb-wiki-course-files.php
+++ templates/template-parts/isceb-wiki-course-files.php
@@ -32,42 +32,47 @@ defined('ABSPATH') || exit;
 
 
         <div class="isceb-wiki-file">
-            <img src="<?php echo plugin_dir_url(dirname(dirname((__FILE__)))) . 'public/img/pdf-icon.svg' ?>" class="isceb-wiki-pdf">
-
+            <div class="isceb-wiki-file-left-part">
+                <img src="<?php echo plugin_dir_url(dirname(dirname((__FILE__)))) . 'public/img/pdf-icon.svg' ?>" class="isceb-wiki-pdf">
+            </div>
             <div class="isceb-wiki-file-meta">
-                <p id="<?php echo $isceb_wiki_course_file->ID ?>" class="isceb-wiki-file-title"> <?php echo $isceb_wiki_course_file->post_title; ?>
+                <p id="<?php echo $isceb_wiki_course_file->ID ?>" class="isceb-wiki-file-title"> 
+                <?php echo $isceb_wiki_course_file->post_title;?>
                 </p>
                 <p class="isceb-wiki-filesize">File size: <?php echo size_format($file_content["filesize"]) ?></p>
 
                 <p class="isceb-wiki-ac-year"> Academic Year: <?php echo $current_academic_year; ?></p>
             </div>
 
-
-            <?php if (is_user_logged_in()) : ?>
-                <a href=" <?php echo $file_content['url'] ?>" download class="isceb-wiki-download-wrap">
-                    <button class="isceb-wiki-file-download isceb-wiki-button-not-gb">Download</button>
-                </a>
-                <?php else :
-                $isceb_wiki_login_page = get_exopite_sof_option('isceb_wiki-test');
-                if (isset($isceb_wiki_login_page['isceb_wiki_login_page']) && $isceb_wiki_login_page['isceb_wiki_login_page'] != '') {
-                ?>
-                    <a href=" <?php echo get_page_link($isceb_wiki_login_page['isceb_wiki_login_page']) ?> " class="isceb-wiki-download-wrap">
-                        <button class="isceb-wiki-file-download isceb-wiki-button-not-gb">Login to download file</button>
+            <div class="isceb-wiki-file-right-part">
+                <?php if (is_user_logged_in()) : ?>
+                    <a href=" <?php echo $file_content['url'] ?>" download class="isceb-wiki-download-wrap">
+                        <!-- <button class="isceb-wiki-file-download isceb-wiki-button-not-gb">Download</button> -->
+                        <i class="fas fa-download fa-2x"></i>
                     </a>
+                    
+                    <?php else :
+                    $isceb_wiki_login_page = get_exopite_sof_option('isceb_wiki-test');
+                    if (isset($isceb_wiki_login_page['isceb_wiki_login_page']) && $isceb_wiki_login_page['isceb_wiki_login_page'] != '') {
+                    ?>
+                        <a href=" <?php echo get_page_link($isceb_wiki_login_page['isceb_wiki_login_page']) ?> " class="isceb-wiki-download-wrap">
+                            <button class="isceb-wiki-file-download isceb-wiki-button-not-gb">Login to download file</button>
+                        </a>
 
-                <?php
-                } else {
-                    global $wp;
-                ?>
+                    <?php
+                    } else {
+                        global $wp;
+                    ?>
 
-                    <a href=" <?php echo (wp_login_url(home_url($wp->request))) ?> " class="isceb-wiki-download-wrap">
-                        <button class="isceb-wiki-file-download isceb-wiki-button-not-gb">Login to download file</button>
-                    </a>
+                        <a href=" <?php echo (wp_login_url(home_url($wp->request))) ?> " class="isceb-wiki-download-wrap">
+                            <button class="isceb-wiki-file-download isceb-wiki-button-not-gb">Login to download file</button>
+                        </a>
 
-                <?php
-                }
-                ?>
-            <?php endif; ?>
+                    <?php
+                    }
+                    ?>
+                <?php endif; ?>
+            </div>
         </div>
 
 
