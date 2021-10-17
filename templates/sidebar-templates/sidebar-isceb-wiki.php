423b5004c4ac81125bf11193ef62f5948e51319e
Stijn Pinkhof <stijn.pinkhof@skynet.be>
2021-10-17 16:41:05 +0200

    work on dropwodn

--- templates/sidebar-templates/sidebar-isceb-wiki.php
+++ templates/sidebar-templates/sidebar-isceb-wiki.php
@@ -14,11 +14,18 @@ defined('ABSPATH') || exit;
 <div class="col-md-2 widget-area" id="isceb-wiki-sidebar" role="complementary">
     <div class="isceb-nav-container-sticky">
         <div class="isceb-nav-header">
-            <h4>
-                <a href="<?php echo (get_site_url() . '/wiki') ?>" id="isceb-wiki-nav-header-text">
+
+            <a href="<?php echo (get_site_url() . '/wiki') ?>" id="isceb-wiki-nav-header-text">
+                <h4 style="margin-bottom: 0px;">
                     <i class="fa fa-home" aria-hidden="true"></i>
-                    ISCEB Wiki</a>
-            </h4>
+                    ISCEB Wiki
+                </h4>
+            </a>
+
+            <button id="isceb-wiki-nav-button-toggle" class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#isceb-wiki-navbar-content" aria-controls="isceb-wiki-navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
         </div>
 
         <?php
@@ -27,43 +34,45 @@ defined('ABSPATH') || exit;
         else :
         ?>
 
-            <h4 class="isceb-nav-program-name"> <?php echo $title_of_page ?></h4>
+            <div class="collapse navbar-collapse isceb-wiki-nav-container-supra dont-collapse-sm" id="isceb-wiki-navbar-content">
+                <h4 class="isceb-nav-program-name"> <?php echo $title_of_page ?></h4>
 
-            <div id="isceb-wiki-nav-container">
-                <?php foreach ($wiki_phases  as $phase) :
-                    //Get all the courses for a certain phase
-                    $wiki_courses = get_posts(array(
-                        'post_type' => 'course',
-                        'meta_query' => array(
-                            array(
-                                'key' => 'phases', // name of custom field
-                                'value' => '"' . $phase->ID . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
-                                'compare' => 'LIKE'
+                <div id="isceb-wiki-nav-container">
+                    <?php foreach ($wiki_phases  as $phase) :
+                        //Get all the courses for a certain phase
+                        $wiki_courses = get_posts(array(
+                            'post_type' => 'course',
+                            'meta_query' => array(
+                                array(
+                                    'key' => 'phases', // name of custom field
+                                    'value' => '"' . $phase->ID . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+                                    'compare' => 'LIKE'
+                                )
                             )
-                        )
-                    ));
-                ?>
+                        ));
+                    ?>
 
-                    <div>
-                        <i class="fa fa-sort-desc" aria-hidden="true" id="isceb-wiki-toggle<?php echo $phase->post_title ?>" onclick="isceb_wiki_sidebar_toggle_function('<?php echo ('isceb-wiki-courses-per-phase-' . $phase->post_title) ?>',this)"></i>
-                        <a href="<?php echo get_permalink($phase->ID); ?>" class="isceb-wiki-nav-phase"> <?php echo $phase->post_title; ?> </a>
-                    </div>
-                    <hr class="isceb-wiki-nav-phase-line">
+                        <div>
+                            <i class="fa fa-sort-desc" aria-hidden="true" id="isceb-wiki-toggle<?php echo $phase->post_title ?>" onclick="isceb_wiki_sidebar_toggle_function('<?php echo ('isceb-wiki-courses-per-phase-' . $phase->post_title) ?>',this)"></i>
+                            <a href="<?php echo get_permalink($phase->ID); ?>" class="isceb-wiki-nav-phase"> <?php echo $phase->post_title; ?> </a>
+                        </div>
+                        <hr class="isceb-wiki-nav-phase-line">
 
-                    <ul class="isceb-wiki-courses-per-phase" id="<?php echo ('isceb-wiki-courses-per-phase-' . $phase->post_title) ?>" style="display: block;">
-                        <?php foreach ($wiki_courses  as $course) :
-                        ?>
-                            <li>
-                                <a href="<?php echo get_permalink($course->ID); ?>" class="isceb-wiki-nav-course"> <?php echo $course->post_title; ?> </a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endforeach; ?>
-            </div>
-        <?php
+                        <ul class="isceb-wiki-courses-per-phase" id="<?php echo ('isceb-wiki-courses-per-phase-' . $phase->post_title) ?>" style="display: block;">
+                            <?php foreach ($wiki_courses  as $course) :
+                            ?>
+                                <li>
+                                    <a href="<?php echo get_permalink($course->ID); ?>" class="isceb-wiki-nav-course"> <?php echo $course->post_title; ?> </a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endforeach; ?>
+                </div>
+            <?php
         endif;
-        ?>
-        <button class="isceb-wiki-button-not-gb" id="isceb-wiki-nav-upload-btn"> Upload files </button>
+            ?>
+            <button class="isceb-wiki-button-not-gb" id="isceb-wiki-nav-upload-btn"> Upload files </button>
+            </div>
     </div>
 </div>
 
