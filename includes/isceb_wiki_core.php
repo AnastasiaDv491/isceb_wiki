6ae2df9f0506df515e329cccdde306f268e674a5
AnastasiaDv491 <anadvoryanchikova@gmail.com>
2021-11-08 20:36:49 +0100

    displayign owned user wiki files

--- includes/isceb_wiki_core.php
+++ includes/isceb_wiki_core.php
@@ -597,37 +597,43 @@ function isceb_wiki_get_files_of_owner($user_id)
     ));
 
     // var_dump($owned_wiki_files);
-    foreach ($owned_wiki_files as $owned_wiki_file) {
-        ?> 
-        <h1>This is the data of the file you uploaded: </h1>
-        <h2> <?php echo $owned_wiki_file->post_title?></h2>
-        <?php
+    if (!empty($owned_wiki_files)) {
+        foreach ($owned_wiki_files as $owned_wiki_file) {
+?>
+            <h1>This is the data of the file you uploaded: </h1>
+            <h2> <?php echo $owned_wiki_file->post_title ?></h2>
+            <?php
 
-        $owned_wiki_files_categories = get_the_terms($owned_wiki_file->ID, 'wiki_file_category');
-        $owned_wiki_file_courses = get_field('course', $owned_wiki_file->ID);
+            if (!empty(get_the_terms($owned_wiki_file->ID, 'wiki_file_category'))) {
+                $owned_wiki_files_categories = get_the_terms($owned_wiki_file->ID, 'wiki_file_category');
 
-        // var_dump($owned_wiki_files_categories);
-      
-        foreach ($owned_wiki_files_categories as $owned_wiki_files_category) {
+                if (get_field('course', $owned_wiki_file->ID)) {
+                    $owned_wiki_file_courses = get_field('course', $owned_wiki_file->ID);
 
-?><p><?php echo $owned_wiki_files_category->name; ?></p>
-        <?php
-        }
-        foreach ($owned_wiki_file_courses as $owned_wiki_files_course) {
-        ?><p><?php echo $owned_wiki_files_course->post_title; ?></p>
+                    // var_dump($owned_wiki_files_categories);
+
+                    foreach ($owned_wiki_files_categories as $owned_wiki_files_category) {
+
+            ?><p><?php echo $owned_wiki_files_category->name; ?></p>
+                    <?php
+                    }
+                    foreach ($owned_wiki_file_courses as $owned_wiki_files_course) {
+                    ?><p><?php echo $owned_wiki_files_course->post_title; ?></p>
+                        <?php
+                        $owned_wiki_files_phases = get_field('phases', $owned_wiki_files_course->ID);
+                        foreach ($owned_wiki_files_phases as $owned_wiki_files_phase) {
+                        ?><p><?php echo $owned_wiki_files_phase->post_title; ?></p>
+                            <?php
+
+                            $owned_wiki_files_programs = get_field('program', $owned_wiki_files_phase);
+                            foreach ($owned_wiki_files_programs as $owned_wiki_files_program) {
+                            ?><p><?php echo $owned_wiki_files_program->post_title; ?></p>
 <?php
-            $owned_wiki_files_phases = get_field('phases', $owned_wiki_files_course->ID);
-            foreach ($owned_wiki_files_phases as $owned_wiki_files_phase) {
-                ?><p><?php echo $owned_wiki_files_phase->post_title; ?></p>
-            <?php
-
-                $owned_wiki_files_programs = get_field('program', $owned_wiki_files_phase);
-                foreach ($owned_wiki_files_programs as $owned_wiki_files_program) {
-                    ?><p><?php echo $owned_wiki_files_program->post_title; ?></p>
-            <?php
+                            }
+                        }
+                    }
                 }
             }
-            
         }
     }
 }
