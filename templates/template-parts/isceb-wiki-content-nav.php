75754218d55a74e2f802b66a67e193f398a2cf6c
Stijn P <stijn.pinkhof@skynet.be>
2022-04-09 11:40:35 +0200

    remove post excerpt on course blocks

--- templates/template-parts/isceb-wiki-content-nav.php
+++ templates/template-parts/isceb-wiki-content-nav.php
@@ -20,7 +20,6 @@ defined('ABSPATH') || exit;
         <a href="<?php echo get_permalink($isceb_wiki_nav_item->ID); ?>" class="isceb-grid-phase-item-url">
             <div class="isceb-phase-grid-item">
                 <h6 class="isceb-phase-grid-item-header"> <?php echo $isceb_wiki_nav_item->post_title; ?></h6>
-                <p><?php echo $isceb_wiki_nav_item->post_excerpt ?></p>
             </div>
         </a>
     <?php endforeach; ?>
